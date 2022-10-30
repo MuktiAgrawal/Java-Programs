1236c329471042e173e84d27dca476c1f849c4b1
Mukti Agrawal <muktiagrawal135@gmail.com>
2022-10-30 22:43:09 +0530

    Modified Singly_linkedlist.java

--- LinkedList/Singly_linkedlist.java
+++ LinkedList/Singly_linkedlist.java
@@ -77,4 +77,45 @@ public class Singly_linkedlist {
         System.out.println("Node not found");
         return false;
     }
+    // Deleting a node from Singly Linked List
+    public void delete(int location){
+        if(head==null){
+            System.out.println("Singly linked list does not exist");
+            return;
+        }
+        else if(location==0){
+            head=head.next;
+            size--;
+            if(size==0){
+                tail=null;
+            }
+        }
+        else if(location>=size-1){ 
+            Node tempNode=head;
+            for(int i=0;i<size-2;i++){
+                tempNode=tempNode.next;
+            }
+            if(tempNode==head){
+                tail=head=null;
+                size--;
+                return;
+            }
+            tempNode.next=null;
+            tail=tempNode;
+            size--;
+        }
+        else{
+            Node tempNode=head;
+            for(int i=0;i<location-1;i++){
+                tempNode=tempNode.next;
+            }
+            tempNode.next=tempNode.next.next;
+            size--;
+        }
+    }
+    public void deleteEntireSLL(){
+        head=null;
+        tail=null;
+        System.out.println("SLL deleted successfully");
+    }
 }

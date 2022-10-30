3b5b30cbca7d46abcf1bd0316e461a9ed32dc9c0
Mukti Agrawal <muktiagrawal135@gmail.com>
2022-10-30 22:42:36 +0530

    Updated Main.java

--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -6,10 +6,14 @@ public class Main{
         sll.insert(6,0); // location starts from 0
         sll.insert(8,3);
         sll.insert(9,2);
-        System.out.println(sll.head.value);
-        System.out.println(sll.head.next.value);
-        System.out.println(sll.head.next.next.value);
+        // System.out.println(sll.head.value);
+        // System.out.println(sll.head.next.value);
+        // System.out.println(sll.head.next.next.value);
         sll.traversal();
-        sll.searchNode(9);
+        // sll.searchNode(9);
+        sll.delete(1);
+        sll.traversal();
+        sll.deleteEntireSLL();
+        sll.traversal();
     }
 }

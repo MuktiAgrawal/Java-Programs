a71d6bc465c42c7be71a8d4d2a2a46fd9c8a213b
Mukti Agrawal <muktiagrawal135@gmail.com>
2022-11-04 23:23:54 +0530

    a

---
+++ a.java
@@ -0,0 +1,11 @@
+public class a {
+    public static void main(String arg[]) {
+        int i;
+        for (i = 1; i <= 12; i += 2) {
+            if (i == 8) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}

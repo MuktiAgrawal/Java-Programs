ae63f2e97d6c73476af1dd86717ce20b735e2d1d
Mukti Agrawal <muktiagrawal135@gmail.com>
2022-11-06 00:33:04 +0530

    Added Stack_Using_LL.java

---
+++ Stack/Stack_Using_LL.java
@@ -0,0 +1,43 @@
+public class Stack_Using_LL {
+    Singly_linkedlist sll;
+    public Stack_Using_LL(){
+        sll=new Singly_linkedlist();
+    }
+    // Push method
+    public void push(int value){
+        sll.insert(value,0);
+    }
+    // isEmpty
+    public boolean isEmpty(){
+        if(sll.head==null)
+            return true;
+        return false;
+    }
+    // Pop method
+    public int pop(){
+        int result=-1;
+        if(isEmpty()){
+            System.out.println("Stack is empty");
+        }
+        else{
+            result=sll.head.value;
+            sll.delete(0);
+        }
+        return result;
+    }
+    // Peek method
+    public int peek(){
+        int result=-1;
+        if(isEmpty()){
+            System.out.println("Stack is empty");
+        }
+        else{
+            result=sll.head.value;
+        }
+        return result;
+    }
+    public void deleteStack(){
+        sll.head=null;
+        System.out.println("Stack successfully deleted!");
+    }
+}

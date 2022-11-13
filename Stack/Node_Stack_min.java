f04f8e2bfb61df3fb85cc7f91ce6a01e397fdb8d
Mukti Agrawal <muktiagrawal135@gmail.com>
2022-11-13 23:32:15 +0530

    Added Node_Stack_min.java

---
+++ Stack/Node_Stack_min.java
@@ -0,0 +1,8 @@
+public class Node_Stack_min {
+    int value;
+    Node_Stack_min next;
+    public Node_Stack_min(int value,Node_Stack_min next){
+        this.value=value;
+        this.next=next;
+    }
+}

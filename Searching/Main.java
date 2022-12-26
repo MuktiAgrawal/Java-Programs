55f0d7a3c70a2ddfe1168526ae6c86265bb3db93
Mukti Agrawal <muktiagrawal135@gmail.com>
2022-12-27 00:39:00 +0530

    Added Main.java

---
+++ Searching/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr={1,3,2,22,12,13};
+        int[] b={4,9,13,23,90};
+        LinearSearch.linearsearch(arr,22);
+        BinarySearch.binarysearch(b, 13);
+    }
+}

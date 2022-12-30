6c8e41269b1cdcb62d651e5fe283370d3d9865b3
Mukti Agrawal <muktiagrawal135@gmail.com>
2022-12-31 01:20:47 +0530

    Modified Main.java

--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -2,7 +2,7 @@ public class Main{
     public static void main(String[] args) {
         Singly_linkedlist sll=new Singly_linkedlist();
         sll.createSinglyLinkedList(5);
-        System.out.println(sll.head.value);
+        // System.out.println(sll.head.value);
         sll.insert(6,0); // location starts from 0
         sll.insert(8,3);
         sll.insert(9,2);

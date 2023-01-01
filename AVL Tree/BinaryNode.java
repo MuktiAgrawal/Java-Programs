b2f95190d21e32b714045059c158d6ec10ff0de5
Mukti Agrawal <muktiagrawal135@gmail.com>
2023-01-01 13:29:52 +0530

    Added BinaryNode.java

---
+++ AVL Tree/BinaryNode.java
@@ -0,0 +1,9 @@
+public class BinaryNode {
+    public BinaryNode left;
+    public BinaryNode right;
+    public int value;
+    public int height;
+    BinaryNode(){
+        this.height=0;
+    }   
+}

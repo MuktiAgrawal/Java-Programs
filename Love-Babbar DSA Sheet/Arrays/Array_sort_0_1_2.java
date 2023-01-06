6fe99682b01338f6be50064d7463432b201416ce
Mukti Agrawal <muktiagrawal135@gmail.com>
2023-01-07 02:06:17 +0530

    Modified Arrays_sort_0_1_2.java

--- Love-Babbar DSA Sheet/Arrays/Array_sort_0_1_2.java
+++ Love-Babbar DSA Sheet/Arrays/Array_sort_0_1_2.java
@@ -1,6 +1,8 @@
 /** Program that sorts an array consisting of 0,1 and 2 without using any sorting algo. */
+// Dutch National Flag Problem 
 class Array_sort_0_1_2
 {
+    // Using Pointer Approach
     public void sort012(int a[], int n)
     {
         // code here 
@@ -26,6 +28,35 @@ class Array_sort_0_1_2
             else{
                 m++;
             }
+        }
+    }
+
+    // Using Counting approach
+    public void sort_012(int a[],int n){
+        int arr[]=new int[3];
+        for(int i=0;i<n;i++){
+            if(a[i]==0){
+                arr[0]++;
+            }
+            else if(a[i]==1){
+                arr[1]++;
+            }
+            else{
+                arr[2]++;
+            }
+        }
+        int i=0;
+        while(arr[0]>0){
+            a[i++]=0;
+            arr[0]--;
+        }
+        while(arr[1]>0){
+            a[i++]=1;
+            arr[1]--;
+        }
+        while(arr[2]>0){
+            a[i++]=2;
+            arr[2]--;
         }
     }
 }

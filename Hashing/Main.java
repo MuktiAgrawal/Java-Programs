a77e1fde7267b867d55191b7b05969008996cf43
Mukti Agrawal <muktiagrawal135@gmail.com>
2023-01-26 16:50:22 +0530

    Added Main

---
+++ Hashing/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        DirectChaining directChaining=new DirectChaining(13);
+        directChaining.insertHashTable("The");
+        directChaining.insertHashTable("quick");
+        directChaining.insertHashTable("brown");
+        directChaining.insertHashTable("fox");
+        directChaining.insertHashTable("jumps");
+        directChaining.insertHashTable("over");
+        directChaining.displayHashTable();
+        directChaining.searchHashTable("fox");
+        directChaining.deleteKeyHashTable("over");
+        directChaining.displayHashTable();
+    }
+}

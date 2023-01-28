55eb2ce0b50964a22c764e29b65c80c739e31cd9
Mukti Agrawal <muktiagrawal135@gmail.com>
2023-01-29 00:53:25 +0530

    Modified Bubble Sort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,4 +1,5 @@
 public class BubbleSort {
+    // Best case Complexity O(n*n)
     void bubblesort(int arr[]){
         int len=arr.length;
         for(int i=0;i<len-1;i++){
@@ -9,6 +10,24 @@ public class BubbleSort {
                     arr[j+1]=temp;
                 }
             }
+        }
+    }
+    // Best case Complexity O(n)
+    void bubblesortOptimized(int arr[]){
+        int len=arr.length;
+        int flag;
+        for(int i=0;i<len-1;i++){
+            flag=0;
+            for(int j=0;j<len-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    flag=1;
+                }
+            }
+            if(flag==0)
+                break;
         }
     }
     void printArray(int arr[]){

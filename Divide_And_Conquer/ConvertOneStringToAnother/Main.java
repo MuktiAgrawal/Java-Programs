daca99f43b12ccec56d6a63361a6b5d7c4fa31ba
Mukti Agrawal <muktiagrawal135@gmail.com>
2023-02-07 23:22:16 +0530

    Added Main

---
+++ Divide_And_Conquer/ConvertOneStringToAnother/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        ConvertOneStringToAnother convertTo=new ConvertOneStringToAnother();
+        System.out.println(convertTo.findMinOperations("table","tbres"));
+        
+    }
+}

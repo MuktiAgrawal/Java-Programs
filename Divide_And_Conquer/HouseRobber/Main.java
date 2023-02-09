08ae2b78dd471b7a017d81b18030624b08fa897a
Mukti Agrawal <muktiagrawal135@gmail.com>
2023-02-09 19:09:19 +0530

    Added Main.java

---
+++ Divide_And_Conquer/HouseRobber/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        HouseRobber hr=new HouseRobber();
+        int[] HouseNetWorth={6,7,1,30,8,2,4};
+        System.out.println(hr.maxMoney(HouseNetWorth));
+    }
+}

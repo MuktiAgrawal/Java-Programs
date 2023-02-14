1bdc4b34b913ce09d16396c2148136a0be1cdab2
Mukti Agrawal <muktiagrawal135@gmail.com>
2023-02-14 18:17:32 +0530

    Added Main.java

---
+++ Divide_And_Conquer/LongestCommonSubsequence/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        LongestCommonSubsequence lcs=new LongestCommonSubsequence();
+        System.out.println(lcs.findLCSLength("elephant", "erepat"));
+    }
+}

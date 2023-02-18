21251a0def802c9e254a3f94649ec91b7bc2da3b
Mukti Agrawal <muktiagrawal135@gmail.com>
2023-02-18 09:22:40 +0530

    Added Main file

---
+++ Divide_And_Conquer/LongestPalindromicSubsequence(LPS)/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        LongestPalindromicSubsequence lps=new LongestPalindromicSubsequence();
+        System.out.println("Longest Palindromic Sequence: "+lps.findLPSLength("elrmenmet"));
+    }
+}

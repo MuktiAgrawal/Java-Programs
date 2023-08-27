728a4fc79cb4bb57565b58051c7696fa982f6ca3
Mukti Agrawal <muktiagrawal135@gmail.com>
2023-08-27 15:05:21 +0530

    Added SplitArrayLargestSum

---
+++ SplitArrayLargestSum.java
@@ -0,0 +1,28 @@
+/* https://leetcode.com/problems/split-array-largest-sum/ */
+public class SplitArrayLargestSum {
+    public int splitArray(int[] nums, int k) {
+        int end = 0, start = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            end += nums[i];
+            start = Math.max(nums[i], start);
+        }
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            int sum = 0, pieces = 1;
+            for (int num : nums) {
+                if (sum + num > mid) {
+                    pieces++;
+                    sum = num;
+                } else {
+                    sum += num;
+                }
+            }
+            if (pieces <= k) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return end;
+    }
+}
